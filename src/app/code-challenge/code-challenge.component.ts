2f94912facaa36d867520d7d8adc3628d87bd779
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-09-26 16:02:23 -0400

    almost have mode completely working

--- src/app/code-challenge/code-challenge.component.ts
+++ src/app/code-challenge/code-challenge.component.ts
@@ -15,10 +15,16 @@ export class CodeChallengeComponent implements OnInit {
   items = [1, 2, 3];
   numbers = [];
 
+  correctSort(a , b){
+    return a - b;
+  }
+
   submit($event) {
     if(isNaN(Number($event.target.value)) === false){
-    this.numbers.push(Number($event.target.value));
-    console.log(this.numbers);
+    (this.numbers.push(Number($event.target.value)));
+    this.numbers.sort(this.correctSort)
+    $event.target.value = null;
+    // console.log(this.numbers);
   }
   else{
     alert("That's not a number bro...")
@@ -39,13 +45,13 @@ export class CodeChallengeComponent implements OnInit {
         this.max = this.numbers[i]
       }
     }
-    console.log("The max is: " + this.max)
+    // console.log("The max is: " + this.max)
     this.calcMin();
   }
 
   calcMin(){
     this.min = Math.min.apply(Math, this.numbers)
-    console.log("the min is:" + this.min)
+    // console.log("the min is:" + this.min)
     this.calcMean();
     }
 
@@ -54,7 +60,29 @@ export class CodeChallengeComponent implements OnInit {
       this.total = this.total + this.numbers[i];
     }
     this.mean = this.total / this.numbers.length
-    console.log("the mean is: " + this.mean)
+    // console.log("the mean is: " + this.mean)
+    this.calcMode();
+  }
+
+  topOccurs = 0;
+  occurs = 0;
+  mode=0;
+  holdSpot = 0;
+
+  calcMode() {
+    for (var i = 0; i < this.numbers.length; i++) {
+        // console.log(this.numbers[i]);
+        if( this.numbers[i] === this.numbers[i+1] ){
+          this.occurs = this.occurs + 1;
+        }
+        if(this.occurs > this.topOccurs){
+          this.mode = this.numbers[i];
+          this.topOccurs = this.occurs;
+          this.occurs = 0;
+        }
+      }
+    console.log(this.mode);
+    // this.numbers=[];
   }
 
 
